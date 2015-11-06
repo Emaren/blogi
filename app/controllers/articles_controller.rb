c192d870ebea5e89f209f17081722bc918bfecd3
Tony Blum <tonyblum@me.com>
2015-11-05 21:52:30 -0800

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,39 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  def new
+    @article = Article.new
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])  
+
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to  @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
